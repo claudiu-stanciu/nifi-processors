90656620bc09b97cefb69a4a65dc71fb818e87a8
claudiu stanciu <claudiu.a.stanciu@gmail.com>
2017-07-13 10:34:10 +0200

    Removed Xml validation

--- src/main/java/com/thinkbiganalytics/ConvertXmlToJson.java
+++ src/main/java/com/thinkbiganalytics/ConvertXmlToJson.java
@@ -1,27 +1,27 @@
-package com.thinkbiganalytics;
-
-/*-
- * #%L
- * Copyright (C) 2017 ThinkBig Analytics
- * %%
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- * 
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- * #L%
  */
 
-import java.io.ByteArrayInputStream;
+package com.thinkbiganalytics;
+
+
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.nio.charset.Charset;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashSet;
@@ -29,10 +29,6 @@ import java.util.List;
 import java.util.Set;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicReference;
-
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
 
 import org.apache.commons.io.IOUtils;
 import org.apache.nifi.annotation.behavior.ReadsAttribute;
@@ -55,142 +51,107 @@ import org.json.JSONObject;
 import org.json.XML;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.xml.sax.InputSource;
-import org.xml.sax.SAXException;
 
-@Tags({ "xml", "json", "convert" })
+@Tags({"xml", "json", "convert"})
 @CapabilityDescription("Converts a XML into JSON")
 @ReadsAttribute(attribute = "filename", description = "The filename to use when writing the FlowFile to disk.")
 public class ConvertXmlToJson extends AbstractProcessor {
 
-	final static Logger logger = LoggerFactory.getLogger(ConvertXmlToJson.class);
+    final static Logger logger = LoggerFactory.getLogger(ConvertXmlToJson.class);
 
-	public static final PropertyDescriptor PROP_PP_INDENT_FACTOR = new PropertyDescriptor.Builder()
-			.name("JSON pretty print indent factor").description("Pretty print indent factor").required(false)
-			.addValidator(StandardValidators.INTEGER_VALIDATOR).defaultValue("0").expressionLanguageSupported(true)
-			.build();
+    public static final PropertyDescriptor PRETTY_PRINT_INDENT_FACTOR = new PropertyDescriptor.Builder()
+            .name("JSON pretty print indent factor").description("Pretty print indent factor").required(false)
+            .addValidator(StandardValidators.INTEGER_VALIDATOR).defaultValue("0").expressionLanguageSupported(true)
+            .build();
 
-	public static final Relationship REL_ORIGINAL = new Relationship.Builder().name("original")
-			.description("Original XML file").build();
+    public static final Relationship REL_ORIGINAL = new Relationship.Builder().name("original")
+            .description("Original XML file").build();
 
-	public static final Relationship REL_SUCCESS = new Relationship.Builder().name("success")
-			.description("Successfully converted to JSON").build();
+    public static final Relationship REL_SUCCESS = new Relationship.Builder().name("success")
+            .description("Successfully converted to JSON").build();
 
-	public static final Relationship REL_FAILED = new Relationship.Builder().name("failed")
-			.description("Failed to parse XML file. Original FlowFile will be routed on this path").build();
+    public static final Relationship REL_FAILED = new Relationship.Builder().name("failed")
+            .description("Failed to parse XML file. Original FlowFile will be routed on this path").build();
 
-	private Set<Relationship> relationships;
+    private Set<Relationship> relationships;
 
-	private List<PropertyDescriptor> properties;
+    private List<PropertyDescriptor> properties;
 
-	@Override
-	protected void init(final ProcessorInitializationContext context) {
-		final List<PropertyDescriptor> properties = new ArrayList<>();
-		properties.add(PROP_PP_INDENT_FACTOR);
-		this.properties = Collections.unmodifiableList(properties);
+    @Override
+    protected void init(final ProcessorInitializationContext context) {
+        final List<PropertyDescriptor> properties = new ArrayList<>();
+        properties.add(PRETTY_PRINT_INDENT_FACTOR);
+        this.properties = Collections.unmodifiableList(properties);
 
-		Set<Relationship> relationships = new HashSet<>();
-		relationships.add(REL_ORIGINAL);
-		relationships.add(REL_SUCCESS);
-		relationships.add(REL_FAILED);
-		this.relationships = Collections.unmodifiableSet(relationships);
-	}
+        Set<Relationship> relationships = new HashSet<>();
+        relationships.add(REL_ORIGINAL);
+        relationships.add(REL_SUCCESS);
+        relationships.add(REL_FAILED);
+        this.relationships = Collections.unmodifiableSet(relationships);
+    }
 
-	@Override
-	public Set<Relationship> getRelationships() {
-		return this.relationships;
-	}
+    @Override
+    public Set<Relationship> getRelationships() {
+        return this.relationships;
+    }
 
-	@Override
-	public List<PropertyDescriptor> getSupportedPropertyDescriptors() {
-		return properties;
-	}
+    @Override
+    public List<PropertyDescriptor> getSupportedPropertyDescriptors() {
+        return properties;
+    }
 
-	@Override
-	public void onTrigger(final ProcessContext context, final ProcessSession session) throws ProcessException {
-		final AtomicBoolean failed = new AtomicBoolean(false);
+    @Override
+    public void onTrigger(final ProcessContext context, final ProcessSession session) throws ProcessException {
+        final AtomicBoolean failed = new AtomicBoolean(false);
 
-		final FlowFile originalFlowFile = session.get();
-		if (originalFlowFile == null) {
-			return;
-		}
-		final FlowFile flowFile = session.clone(originalFlowFile);
+        final FlowFile originalFlowFile = session.get();
+        
+        if (originalFlowFile == null || originalFlowFile.getSize() == 0L) {
+            return;
+        }
+        final FlowFile flowFile = session.clone(originalFlowFile);
 
-		session.transfer(originalFlowFile, REL_ORIGINAL);
+        session.transfer(originalFlowFile, REL_ORIGINAL);
 
-		if (flowFile.getSize() > 0L) {
-			final AtomicReference<String> jsonObjVal = new AtomicReference<>();
-			session.read(flowFile, new InputStreamCallback() {
-				@Override
-				public void process(InputStream in) throws IOException {
-					try {
-						final int prettyPrintIndent = context.getProperty(PROP_PP_INDENT_FACTOR)
-								.evaluateAttributeExpressions(originalFlowFile).asInteger();
+        final AtomicReference<String> jsonObjVal = new AtomicReference<>();
+        session.read(flowFile, new InputStreamCallback() {
+            @Override
+            public void process(InputStream in) throws IOException {
+                try {
+                    final int prettyPrintIndent = context.getProperty(PRETTY_PRINT_INDENT_FACTOR)
+                            .evaluateAttributeExpressions(flowFile).asInteger();
 
-						String xml = IOUtils.toString(in);
-						if (!validXml(xml)) {
-							failed.set(true);
-						} else {
-							try {
+                    String xml = IOUtils.toString(in, Charset.defaultCharset());
 
-								JSONObject xmlJSONObj = XML.toJSONObject(xml);
+                    JSONObject xmlJSONObj = XML.toJSONObject(xml);
 
-								jsonObjVal.set(xmlJSONObj.toString(prettyPrintIndent));
+                    jsonObjVal.set(xmlJSONObj.toString(prettyPrintIndent));
+                    getLogger().debug(jsonObjVal.get());
+                    if (jsonObjVal.get().isEmpty()) {
+                        failed.set(true);
+                    }
 
-							} catch (JSONException ex) {
-								ex.printStackTrace();
-								getLogger().error(
-										"Failed to parse file " + flowFile.getAttribute(CoreAttributes.FILENAME.key())
-												+ " due to " + ex.toString() + ". Routing to failure");
-								failed.set(true);
-							}
-						}
-					} catch (Exception ex) {
-						ex.printStackTrace();
-						getLogger().error("Failed to parse file " + flowFile.getAttribute(CoreAttributes.FILENAME.key())
-								+ " due to " + ex.toString() + ". Routing to failure");
-						failed.set(true);
-					}
-				}
-			});
-			if (!failed.get()) {
-				// Write JSON content to FlowFile
-				FlowFile flowFileJson = session.write(flowFile, new OutputStreamCallback() {
-					@Override
-					public void process(OutputStream out) throws IOException {
-						out.write(jsonObjVal.get().getBytes());
-					}
-				});
-				session.transfer(flowFileJson, REL_SUCCESS);
-			} else {
-				session.transfer(flowFile, REL_FAILED);
-			}
-		} else {
-			session.transfer(flowFile, REL_SUCCESS);
-		}
-		session.commit();
+                } catch (Exception ex) {
+                    ex.printStackTrace();
+                    getLogger().error("Failed to parse to JSON due to " + ex.toString() + ". Routing to failure");
+                    failed.set(true);
+                }
+            }
+        });
 
-	}
+        if (!failed.get()) {
+            // Write JSON content to FlowFile
+            FlowFile flowFileJson = session.write(flowFile, new OutputStreamCallback() {
+                @Override
+                public void process(OutputStream out) throws IOException {
+                    out.write(jsonObjVal.get().getBytes());
+                }
+            });
+            session.transfer(flowFileJson, REL_SUCCESS);
+        } else {
+            session.transfer(flowFile, REL_FAILED);
+        }
 
-	public static boolean validXml(String xml) {
-		boolean valid = false;
+    }
 
-		DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-		factory.setValidating(false);
-		factory.setNamespaceAware(true);
-
-		DocumentBuilder builder;
-
-		// the "parse" method validates the XML, will throw an exception if
-		// misformatted
-		try {
-			builder = factory.newDocumentBuilder();
-			builder.parse(new InputSource(new ByteArrayInputStream(xml.getBytes("utf-8"))));
-			valid = true;
-		} catch (ParserConfigurationException | SAXException | IOException ex) {
-			ex.printStackTrace();
-			logger.error(ex.getMessage());
-		}
-		return valid;
-	}
 }
--- src/test/java/com/thinkbiganalytics/TestConvertXmlToJson.java
+++ src/test/java/com/thinkbiganalytics/TestConvertXmlToJson.java
@@ -7,10 +7,7 @@ import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import static org.junit.Assert.*;
-
 import java.io.IOException;
-import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
@@ -19,11 +16,11 @@ public class TestConvertXmlToJson {
 	final static Logger logger = LoggerFactory.getLogger(TestConvertXmlToJson.class);
 
 	@Test
-	public void testParseSmallXml() throws IOException {
-		final Path XML_SNIPPET = Paths.get("src/test/resources/TestConvertXmlToJson/snippet-small.xml");
-		final Path JSON_SNIPPET = Paths.get("src/test/resources/TestConvertXmlToJson/snippet-small.json");
+	public void testBundledXml() throws IOException {
+		final Path XML_SNIPPET = Paths.get("src/test/resources/TestConvertXmlToJson/snippet-bundle.xml");
+		final Path JSON_SNIPPET = Paths.get("src/test/resources/TestConvertXmlToJson/snippet-bundle.json");
 		final TestRunner testRunner = TestRunners.newTestRunner(new ConvertXmlToJson());
-		testRunner.setProperty(ConvertXmlToJson.PROP_PP_INDENT_FACTOR, "5");
+		testRunner.setProperty(ConvertXmlToJson.PRETTY_PRINT_INDENT_FACTOR, "5");
 		testRunner.enqueue(XML_SNIPPET);
 		testRunner.run();
 		testRunner.assertTransferCount(ConvertXmlToJson.REL_ORIGINAL, 1);
@@ -34,10 +31,23 @@ public class TestConvertXmlToJson {
 	}
 
 	@Test
+	public void testWithEmptyXml() throws IOException {
+		final Path XML_SNIPPET = Paths.get("src/test/resources/TestConvertXmlToJson/snippet-bundle.xml");
+		final Path XML_SNIPPET_EMPTY = Paths.get("src/test/resources/TestConvertXmlToJson/snippet-empty.xml");
+		final TestRunner testRunner = TestRunners.newTestRunner(new ConvertXmlToJson());
+		testRunner.enqueue(XML_SNIPPET);
+		testRunner.enqueue(XML_SNIPPET_EMPTY);
+		testRunner.run();
+		testRunner.assertTransferCount(ConvertXmlToJson.REL_ORIGINAL, 1);
+		testRunner.assertTransferCount(ConvertXmlToJson.REL_SUCCESS, 1);
+		testRunner.assertTransferCount(ConvertXmlToJson.REL_FAILED, 0);
+	}
+
+	@Test
 	public void testFailedParse() throws IOException {
 		final Path XML_SNIPPET = Paths.get("src/test/resources/TestConvertXmlToJson/snippet-malformatted.xml");
 		final TestRunner testRunner = TestRunners.newTestRunner(new ConvertXmlToJson());
-		testRunner.setProperty(ConvertXmlToJson.PROP_PP_INDENT_FACTOR, "0");
+		testRunner.setProperty(ConvertXmlToJson.PRETTY_PRINT_INDENT_FACTOR, "0");
 		testRunner.enqueue(XML_SNIPPET);
 		testRunner.run();
 		testRunner.assertTransferCount(ConvertXmlToJson.REL_ORIGINAL, 1);
@@ -46,26 +56,11 @@ public class TestConvertXmlToJson {
 	}
 
 	@Test
-	public void testBundledXml() throws IOException {
-		final Path XML_SNIPPET = Paths.get("src/test/resources/TestConvertXmlToJson/snippet-bundle.xml");
-		final Path JSON_SNIPPET = Paths.get("src/test/resources/TestConvertXmlToJson/snippet-bundle.json");
-		final TestRunner testRunner = TestRunners.newTestRunner(new ConvertXmlToJson());
-		testRunner.setProperty(ConvertXmlToJson.PROP_PP_INDENT_FACTOR, "5");
-		testRunner.enqueue(XML_SNIPPET);
-		testRunner.run();
-		testRunner.assertTransferCount(ConvertXmlToJson.REL_ORIGINAL, 1);
-		testRunner.assertTransferCount(ConvertXmlToJson.REL_SUCCESS, 1);
-		testRunner.assertTransferCount(ConvertXmlToJson.REL_FAILED, 0);
-		final MockFlowFile flowFile = testRunner.getFlowFilesForRelationship(ConvertXmlToJson.REL_SUCCESS).get(0);
-		flowFile.assertContentEquals(JSON_SNIPPET);
-	}
-
-	@Test
 	public void testWitsml() throws IOException {
 		final Path XML_SNIPPET = Paths.get("src/test/resources/TestConvertXmlToJson/snippet-witsml.xml");
 		final Path JSON_SNIPPET = Paths.get("src/test/resources/TestConvertXmlToJson/snippet-witsml.json");
 		final TestRunner testRunner = TestRunners.newTestRunner(new ConvertXmlToJson());
-		testRunner.setProperty(ConvertXmlToJson.PROP_PP_INDENT_FACTOR, "5");
+		testRunner.setProperty(ConvertXmlToJson.PRETTY_PRINT_INDENT_FACTOR, "5");
 		testRunner.enqueue(XML_SNIPPET);
 		testRunner.run();
 		testRunner.assertTransferCount(ConvertXmlToJson.REL_ORIGINAL, 1);
@@ -73,21 +68,5 @@ public class TestConvertXmlToJson {
 		testRunner.assertTransferCount(ConvertXmlToJson.REL_FAILED, 0);
 		final MockFlowFile flowFile = testRunner.getFlowFilesForRelationship(ConvertXmlToJson.REL_SUCCESS).get(0);
 		flowFile.assertContentEquals(JSON_SNIPPET);
-	}
-
-	@Test
-	public void testInvalidXml() {
-		final Path XML_SNIPPET = Paths.get("src/test/resources/TestConvertXmlToJson/snippet-invalid.xml");
-		try {
-			String content = new String(Files.readAllBytes(XML_SNIPPET));
-			//ConvertXmlToJson convert = new ConvertXmlToJson();
-			//assertTrue();
-			boolean valid = ConvertXmlToJson.validXml(content);
-			assertEquals(false, valid);
-			
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-
 	}
 }
